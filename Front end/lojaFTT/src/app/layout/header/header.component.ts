441bed27140b09eeb09830c1cc8c515a682d2b7b
Victor Macêdo <joaovictormacedo.santos99@gmail.com>
2021-02-11 10:22:51 -0300

    Exercicio FTT

---
+++ Front end/lojaFTT/src/app/layout/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+
+    // let width = window.innerWidth;
+
+    // if(width <= 360) {
+    //   const menuHamburger = document.querySelector('.logo');
+    //   menuHamburger.
+    // }
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
